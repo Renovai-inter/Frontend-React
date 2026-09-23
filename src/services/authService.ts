7bd41a4e5fa5a55bf992f1f90214625d0548c898
Guilhrme-Norcia <guilhermenorcia@gmail.com>
2026-09-23 08:55:45 -0300

    fx: atualizando o authService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -10,23 +10,11 @@ export interface LoginResponse {
     tipo: string;
     email: string;
     role: string;
+    usuarioId: string;
 }
 
-const dadosFicticios: LoginResponse = {
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
-    tipo: "Bearer",
-    email: "maria@novaesperanca.coop.br",
-    role: "admin"
-};
 
 export async function login(data: LoginRequest): Promise<LoginResponse> {
-    //const response = await api.post<LoginResponse>("/auth/login", data);
-    //return response.data;
-
-    // Simulando uma resposta de login bem-sucedida, enquanto o backend não está rodando 
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(dadosFicticios);
-        }, 1000);   
-    });
+    const response = await api.post<LoginResponse>("/auth/login", data);
+    return response.data;    
 };
